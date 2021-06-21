126008039c8519bae9955e148c9bbda9bea2d010
mamcdonald94 <mamcdonald94@gmail.com>
2021-06-21 16:49:18 -0400

    inital commit

---
+++ django_fundamentals/first_django_project/first_django_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return HttpResponse("")
