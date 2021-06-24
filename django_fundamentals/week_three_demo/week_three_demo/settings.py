7548d9e50932017a25668840297a4d1e3fdf10a2
mamcdonald94 <mamcdonald94@gmail.com>
2021-06-23 20:17:19 -0400

    fixed some chinks in the ol armor

--- django_fundamentals/week_three_demo/week_three_demo/settings.py
+++ django_fundamentals/week_three_demo/week_three_demo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from . import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_)zt=@cd3v6x81yxh71a)m%w=9j3xpneozd%$i3wa8@cuwgqdt'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
