6921fae7be47459234ae114e8776faf1cb087be0
Mark McDonald <mamcdonald94@gmail.com>
2021-07-05 10:54:28 -0400

    final changes for updated assignment

--- django_fundamentals/dojos_and_ninjas/dojo_ninjas_app/urls.py
+++ django_fundamentals/dojos_and_ninjas/dojo_ninjas_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('add_dojo', views.dojo),
+    path('add_ninja', views.ninja),
 ]
--- django_fundamentals/dojos_and_ninjas/dojo_ninjas_app/views.py
+++ django_fundamentals/dojos_and_ninjas/dojo_ninjas_app/views.py
@@ -1,5 +1,27 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
+from .models import Dojo, Ninja
 
 
 def index(request):
-    pass
+    context = {
+        'dojos': Dojo.objects.all(),
+        'ninjas': Ninja.objects.all(),
+    }
+    return render(request, 'index.html', context)
+
+def dojo(request):
+    dojo = Dojo.objects.create(
+        name = request.POST['name'],
+        city = request.POST['city'],
+        state = request.POST['state']
+    )
+    return redirect('/')
+
+def ninja(request):
+    ninja = Ninja.objects.create(
+        first_name = request.POST['fname'],
+        last_name = request.POST['lname'],
+        dojo = Dojo.objects.get(id=request.POST['dojo'])
+    )
+    return redirect('/')
