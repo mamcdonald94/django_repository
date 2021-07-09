ecd137c2f4d4b50f00aa2a720b9b179b4bb55af6
mamcdonald94 <mamcdonald94@gmail.com>
2021-07-09 16:24:54 -0400

    did all the queries, in comments in views.py

--- django_orm/sports_orm/leagues/views.py
+++ django_orm/sports_orm/leagues/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import League, Team, Player
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from . import team_maker
 
@@ -19,7 +19,9 @@ def index(request):
 		# "leagues": League.objects.filter(name__icontains="conference"),
 		# find all leagues in the Atlantic region
 		# "leagues": League.objects.filter(name__icontains="atlantic"),
-		"teams": Team.objects.all(),
+		# all leagues with a current player named Sophia
+		# "leagues": League.objects.filter(teams__curr_players__first_name__icontains="Sophia"),
+		# "teams": Team.objects.all(),
 		# find all teams in Dallas
 		# "teams": Team.objects.filter(location__icontains='dallas'),
 		# all teams named raptors
@@ -32,7 +34,25 @@ def index(request):
 		# "teams": Team.objects.order_by('location'),
 		# all teams, ordered by team name in reverse alphabetical order
 		# "teams": Team.objects.order_by('team_name').reverse(),
-		"players": Player.objects.all(),
+		# all teams in the Atlantic Soccer conference
+		# "teams": Team.objects.filter(league__name='Atlantic Soccer Conference'),
+		# all teams with a current player named Sophia
+		# "teams": Team.objects.filter(curr_players__first_name__icontains="Sophia"),
+		# all teams, past and present that Samuel Evans has played with
+		# "teams": Team.objects.filter(Q(all_players__first_name__icontains="Samuel"), Q(all_players__last_name__icontains="Evans")),
+		# every team that Jacob Gray played for before he joined the Oregon Colts
+		# "teams": Team.objects.filter(Q(all_players__first_name__icontains="Jacob"), Q(all_players__last_name__icontains="Gray"), ~Q(team_name__iexact="Colts"), ~Q(location__iexact="Oregon")),
+		# all teams that have had 12 or more players, past and present
+		"teams": Team.objects.annotate(num_players=Count('all_players')).filter(num_players__gt=12),
+		# "players": Player.objects.all(),
+		# all current players on the Boston Penguins
+		# "players": Player.objects.filter(Q(curr_team__team_name="Penguins"), Q(curr_team__location="Boston")),
+		#  all current players in the International Collegiate Baseball Conference
+		# "players": Player.objects.filter(curr_team__league__name="International Collegiate Baseball Conference"),
+		# all current players in the American Conference of Amateur Football w/ last name Lopez
+		# "players": Player.objects.filter(Q(curr_team__league__name="American Conference of Amateur Football"), Q(last_name__iexact="Lopez")),
+		# all football players
+		# "players": Player.objects.filter(curr_team__league__sport__icontains="football"),
 		# every player with last name "Cooper"
 		# "players": Player.objects.filter(last_name__iexact="cooper"),
 		# every player with first name "Joshua"
@@ -41,7 +61,18 @@ def index(request):
 		# "players": Player.objects.filter(last_name__iexact="cooper").exclude(first_name__iexact="joshua"),
 		# all players with first name "Alexander" OR first name "Wyatt"
 		# "players": Player.objects.filter(Q(first_name__iexact="alexander") | Q(first_name__iexact="wyatt")),
+		# everyone with the last name Flores who does not currently play for the Washington Roughriders
+		# "players": Player.objects.filter(Q(last_name__icontains="Flores"), ~Q(curr_team__team_name__icontains='Roughriders')),
+		# all players, past and present, with the Manitoba Tiger-Cats
+		# "players": Player.objects.filter(all_teams__team_name__iexact="Tiger-Cats"),
+		# all players who were formerly (but aren't currently) with the Wichita Vikings
+		# "players": Player.objects.filter(Q(all_teams__team_name__iexact="Vikings"), Q(all_teams__location__iexact="Wichita"), ~Q(curr_team__team_name__iexact="Vikings"), ~Q(curr_team__location__iexact="Wichita")),
+		# everyone named "Joshua" who has ever played in the Atlantic Federation of Amateur Baseball Players
+		# "players": Player.objects.filter(Q(first_name__iexact="joshua"), Q(all_teams__league__name__icontains="Amateur Baseball Players")),
+		# all players and count of teams played for, sorted by the number of teams they've played for
+		"players": Player.objects.annotate(num_teams=Count('all_teams')).order_by('-num_teams'),
 	}
+
 	return render(request, "leagues/index.html", context)
 
 def make_data(request):
