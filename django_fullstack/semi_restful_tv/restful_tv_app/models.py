03389632cfe44b90bd96c0a9d6a9a341f431dfdd
mamcdonald94 <mamcdonald94@gmail.com>
2021-07-13 11:33:15 -0400

    added basic validation to show creation/editing

--- django_fullstack/semi_restful_tv/restful_tv_app/models.py
+++ django_fullstack/semi_restful_tv/restful_tv_app/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # if len(postData['title']) == 0:
+        #     errors['title'] = 'Whoops! You forgot to put in the show title!'
+        # if len(postData['network']) == 0:
+        #     errors['network'] = 'Whoops! You forgot to put in the network name!'
+        # if len(postData['release_date']) == 0:
+        #     errors['release_date'] = 'Whoops! You forgot to put in the release date!'
+        # if len(postData['description']) == 0:
+        #     errors['description'] = 'Whoops! You forgot to put in a description!'
+        if len(postData['title']) < 2:
+            errors['title'] = 'Show title must be at least two characters long'
+        if len(postData['network']) < 3:
+            errors['network'] = 'Network must be at least three characters long'
+        if len(postData['description']) < 10:
+            errors['description'] = 'Description must be at least 10 characters long'
+        return errors
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=45)
@@ -7,3 +26,4 @@ class Show(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    objects = ShowManager()
--- django_fullstack/semi_restful_tv/restful_tv_app/views.py
+++ django_fullstack/semi_restful_tv/restful_tv_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
 from .models import Show
 
 def shows(request):
@@ -8,16 +9,24 @@ def shows(request):
     return render(request, 'shows.html', context)
 
 def new(request):
-    return render(request, 'new.html')
+    return render(request, 'new_show.html')
 
 def create_show(request):
-    this_show = Show.objects.create(
-        title = request.POST['title'],
-        network = request.POST['network'],
-        release_date = request.POST['release_date'],
-        description = request.POST['description']
-    )
-    return redirect(f'/shows/{this_show.id}')
+
+    errors = Show.objects.basic_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+        this_show = Show.objects.create(
+            title = request.POST['title'],
+            network = request.POST['network'],
+            release_date = request.POST['release_date'],
+            description = request.POST['description']
+        )
+        return redirect(f'/shows/{this_show.id}')
 
 def show_info(request, id):
     context = {
@@ -32,12 +41,20 @@ def edit_show(request, id):
     return render(request, "edit_show.html", context)
 
 def update(request, id):
+
+    errors = Show.objects.basic_validator(request.POST)
     show1 = Show.objects.get(id=id)
-    show1.title = request.POST['title']
-    show1.network = request.POST['network']
-    show1.description = request.POST['description']
-    show1.save()
-    return redirect(f'/shows/{show1.id}')
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{show1.id}/edit')
+    else:
+        show1.title = request.POST['title']
+        show1.network = request.POST['network']
+        show1.description = request.POST['description']
+        show1.save()
+        return redirect(f'/shows/{show1.id}')
 
 def delete(request, id):
     show1 = Show.objects.get(id=id)
