b43af19655c0550ad7678b8b4d9507000a0bd3c9
Mark McDonald <mamcdonald94@gmail.com>
2021-07-14 20:11:47 -0400

    some changed validations working with Will

--- django_fullstack/semi_restful_tv/restful_tv_app/models.py
+++ django_fullstack/semi_restful_tv/restful_tv_app/models.py
@@ -1,22 +1,28 @@
 from django.db import models
+from datetime import datetime
 
 class ShowManager(models.Manager):
     def basic_validator(self, postData):
         errors = {}
-        # if len(postData['title']) == 0:
-        #     errors['title'] = 'Whoops! You forgot to put in the show title!'
-        # if len(postData['network']) == 0:
-        #     errors['network'] = 'Whoops! You forgot to put in the network name!'
-        # if len(postData['release_date']) == 0:
-        #     errors['release_date'] = 'Whoops! You forgot to put in the release date!'
-        # if len(postData['description']) == 0:
-        #     errors['description'] = 'Whoops! You forgot to put in a description!'
-        if len(postData['title']) < 2:
+        if postData['title'] == '':
+            errors['title'] = 'Whoops! You forgot to put in the show title!'
+        elif len(postData['title']) < 2:
             errors['title'] = 'Show title must be at least two characters long'
+        if len(postData['network']) == 0:
+            errors['network'] = 'Whoops! You forgot to put in the network name!'
+        if len(postData['release_date']) == 0:
+            errors['release_date'] = 'Whoops! You forgot to put in the release date!'
+        if len(postData['description']) == 0:
+            errors['description'] = 'Whoops! You forgot to put in a description!'
+        # if len(postData['title']) < 2:
+        #     errors['title'] = 'Show title must be at least two characters long'
         if len(postData['network']) < 3:
             errors['network'] = 'Network must be at least three characters long'
+        # if datetime.strptime(postData['release_date'], '%Y-%m-%d') > datetime.now():
+        #     errors['release_date'] = 'Release date must be in the past!'
         if len(postData['description']) < 10:
             errors['description'] = 'Description must be at least 10 characters long'
+        print(postData)
         return errors
 
 class Show(models.Model):
