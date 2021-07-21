15f2b24ea28a1c6563a846a67877aa3ccfaf930b
mamcdonald94 <mamcdonald94@gmail.com>
2021-07-21 15:40:41 -0400

    test

--- django_fullstack/login_registration/login_app/models.py
+++ django_fullstack/login_registration/login_app/models.py
@@ -1,22 +1,17 @@
 from django.db import models
 import re
+import bcrypt
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class UserManager(models.Manager):
     def validator(self, postData):
         errors = {}
-        if postData['first_name'] == '':
-            errors['first_name'] = 'Whoops! You forgot to put in your first name!'
-        elif len(postData['first_name']) < 2:
+        if len(postData['first_name']) < 2:
             errors['first_name'] = 'First name must be at least two characters'
-        if postData['last_name'] == '':
-            errors['last_name'] = 'Whoops! You forgot to put in your last name!'
-        elif len(postData['last_name']) < 2:
+        if len(postData['last_name']) < 2:
             errors['last_name'] = 'Last name must be at least two characters'
-        if postData['email'] == '':
-            errors['email'] = 'Whoops! You forgot to put in your email!'
-        elif len(postData['email']) < 2:
+        if len(postData['email']) < 2:
             errors['email'] = 'First name must be at least two characters'
         if not EMAIL_REGEX.match(postData['email']):   
             errors['email'] = "Invalid email address!"
